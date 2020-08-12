e6da8b244461b458bff651a3f84010a2a7f24f77
Yuan <gn2481@gmail.com>
2020-08-12 14:50:41 +0800

    before_drag

--- 03_to-do-list/main.js
+++ 03_to-do-list/main.js
@@ -1,1 +1,36 @@
 // TO DO
+window.addEventListener('DOMContentLoaded', function () {
+
+  // 新增任務
+  document.querySelector('.addBtn').addEventListener('click', function () {
+    // 新增任務
+    const task = document.createElement('li')
+    task.textContent = document.querySelector('#input').value
+
+    const closeBtn = document.createElement('span')
+    closeBtn.textContent = "x"
+    closeBtn.classList.add('close')
+
+    document.querySelector('ul').appendChild(task)
+    document.querySelector('li:last-child').appendChild(closeBtn)
+
+  })
+
+  document.querySelector('ul').addEventListener('click', function (params) {
+    // console.log(e.target)
+    const target =  params.target
+    if (target.className == 'close') {
+      // 移除任務
+      target.parentNode.remove()
+    } else {
+      // 完成任務
+      target.classList.toggle('checked')
+    }
+  })
+// 監聽在ul上，然後用環境參數params中的target把滑鼠點在哪裡抓出來，再去做判斷
+
+
+
+
+
+})
--- 04_bonus-drag/main.js
+++ 04_bonus-drag/main.js
@@ -1,34 +1,46 @@
 document.addEventListener("DOMContentLoaded", function(){
   let circle = document.querySelector('#circle')
   let target = document.querySelector('#target')
-
+  
   //source
-  circle.addEventListener('drag', function(){
+  circle.addEventListener('drag', function(event){
+    const touched = event.target
     console.log('drag')
+    touched.style.opacity = .5
   })
 
-  circle.addEventListener('dragstart', function(){
+  circle.addEventListener('dragstart', function(event){
     console.log('dragstart')
   })
 
-  circle.addEventListener('dragend', function(){
+  circle.addEventListener('dragend', function(event){
+    const touched = event.target
     console.log('dragend')
+    touched.style.opacity = ""
+    if (touched.className == 'in_target') {
+      target.insertAdjacentElement("beforeend",circle)
+    }
+
+    // 直接指定到框框裡，拖不回來
   })
 
-  // //target
-  // target.addEventListener('dragenter', function(){
-  //   console.log('dragenter target')
-  // })
+  //target
+  target.addEventListener('dragenter', function(){
+    console.log('dragenter target')
+    circle.classList.add('in_target')
+  })
 
-  // target.addEventListener('dragover', function(){
-  //   console.log('dragover target')
-  // })
+  target.addEventListener('dragover', function(){
+    console.log('dragover target')
+  })
 
-  // target.addEventListener('dragleave', function(){
-  //   console.log('dragleave target')
-  // })
+  target.addEventListener('dragleave', function(){
+    console.log('dragleave target')
+    circle.classList.remove('in_target')
 
-  // target.addEventListener('drop', function(){
-  //   console.log('drop')
-  // })
+  })
+
+  target.addEventListener('drop', function(){
+    console.log('drop')
+  })
 })
