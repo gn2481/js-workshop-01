9ee3c0bbb179627a64d903946db9be966621c437
Yuan <gn2481@gmail.com>
2020-08-12 16:42:42 +0800

    basic

--- 03_to-do-list/main.js
+++ 03_to-do-list/main.js
@@ -1,24 +1,25 @@
 // TO DO
 window.addEventListener('DOMContentLoaded', function () {
-
+  var ulist = document.querySelector('ul')
   // 新增任務
   document.querySelector('.addBtn').addEventListener('click', function () {
     // 新增任務
     const task = document.createElement('li')
     task.textContent = document.querySelector('#input').value
+    task.setAttribute('draggable', 'true')
 
     const closeBtn = document.createElement('span')
     closeBtn.textContent = "x"
     closeBtn.classList.add('close')
 
-    document.querySelector('ul').appendChild(task)
+    ulist.appendChild(task)
     document.querySelector('li:last-child').appendChild(closeBtn)
 
   })
 
-  document.querySelector('ul').addEventListener('click', function (params) {
+  ulist.addEventListener('click', function (params) {
     // console.log(e.target)
-    const target =  params.target
+    const target = params.target
     if (target.className == 'close') {
       // 移除任務
       target.parentNode.remove()
@@ -26,9 +27,31 @@ window.addEventListener('DOMContentLoaded', function () {
       // 完成任務
       target.classList.toggle('checked')
     }
+    
   })
-// 監聽在ul上，然後用環境參數params中的target把滑鼠點在哪裡抓出來，再去做判斷
+  // 監聽在ul上，然後用環境參數params中的target把滑鼠點在哪裡抓出來，再去做判斷
 
+  document.querySelectorAll('li').forEach(function (draggedList) {
+    draggedList.addEventListener('drag', function (event) {
+      // console.log(event.screenX)
+      // console.log(event.clientX)
+
+      const current_elm = document.elementFromPoint(event.clientX, event.clientY)
+      
+      const last_elm = document.querySelector('ul li:last-child')
+
+
+      if (current_elm.parentNode.tagName == "UL") {
+        current_elm.insertAdjacentElement('beforebegin', draggedList)
+      }
+
+
+
+
+      // 將滑鼠移到的地方，與draggedList換
+
+    })
+  })
 
 
 
